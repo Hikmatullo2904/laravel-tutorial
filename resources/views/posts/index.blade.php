6473241ba7fc056a8ee773490da8c2dd11c3f751
hikmatullo <hikmatulloanvarbekov001@gmail.com>
2024-10-25 09:12:45 +0500

    post create done

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,18 @@
 
 <x-layout>
     <h1 class="title">Latest Posts</h1>
+    <div class="grid grid-cols-2 gap-6">
+        @foreach($posts as $post)
+            <h2 class="font-bold text-xl">{{$post ->title}}</h2>
+            <div class="text-xs font-light mb-4">
+                <span class="font-semibold">Posted {{$post -> created_at -> diffForHumans()}} By: </span>
+                <a href="" class="text-blue-500 hover:text-blue-600">Username</a>
+            </div>
 
-    @foreach($posts as $post)
-        <h2 class="font-bold text-xl">{{$post ->title}}</h2>
-        <div class="text-xs font-light mb-4">
-            <span class="font-semibold">Posted By: </span>
-            <a href="" class="text-blue-500 hover:text-blue-600"><Username></Username></a>
-        </div>
-
-        <p>{{$post->body}}</p>
-    @endforeach
+            <p>{{Str::words($post->body, 10)}}</p>
+        @endforeach
+    </div>
+    <div>
+        {{$posts->links()}}
+    </div>
 </x-layout>
--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -1,3 +1,31 @@
 <x-layout>
     <h1 class="title">Hello Mr.{{auth()->user()->name}}</h1>
+    <div class="card mb-4">
+        <h2 class="font-bold mb-4">Create a new Post</h2>
+
+        @if(session('success'))
+            <div>
+                <p>{{session('success')}}</p>
+            </div>
+        @endif
+
+        <form action="{{route('posts.store')}}" method="post">
+            @csrf
+            <div class="mb-4">
+                <label for="title" class="label">Title</label>
+                <input type="text" name="title" id="title" class="input">
+                @error('title')
+                {{$message}}
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label for="body" class="label">Body</label>
+                <textarea name="body" id="body" class="input" rows="5">{{old('body')}}</textarea>
+                @error('body')
+                {{$message}}
+                @enderror
+            </div>
+            <button type="submit" class="btn">Submit</button>
+        </form>
+    </div>
 </x-layout>
